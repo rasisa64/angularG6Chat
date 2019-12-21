c20e37be260f2733956c8393d7e783f16b0ed343
aoterolavi <aaronoterolavi@gmail.com>
2019-12-21 12:24:42 -0500

    change enviroment add fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9iueAfPhMT1iUa3EKiWVq5mOu1c6_8bU",
+    authDomain: "angularrandy.firebaseapp.com",
+    databaseURL: "https://angularrandy.firebaseio.com",
+    projectId: "angularrandy",
+    storageBucket: "angularrandy.appspot.com",
+    messagingSenderId: "606623444239",
+    appId: "1:606623444239:web:08fd6605016936da71e27a"
+  } 
 };
